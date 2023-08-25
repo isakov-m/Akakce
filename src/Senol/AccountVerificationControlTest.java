da23ae1b5dddaa92143861d04e55c94021299b21
DoganSenol <senoldogan2018@gmail.com>
2023-08-25 16:04:43 +0200

    AccountVerificationControlTest CreateAccount LogOutTest MessagingBoxCheck OrderBoxCheck dosyalari eklendi

---
+++ src/Senol/AccountVerificationControlTest.java
@@ -0,0 +1,42 @@
+package Senol;
+
+import Utlity.BaseDriver;
+import Utlity.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class AccountVerificationControlTest extends BaseDriver {
+
+    @Test
+
+    public void Login() {
+
+        driver.get("https://www.akakce.com/");
+        MyFunc.Bekle(2);
+
+        WebElement loginBegin = driver.findElement(By.xpath(" //div[@id='H_rl_v8']/a[2]"));
+        loginBegin.click();
+        MyFunc.Bekle(1);
+
+        WebElement TypeEmail = driver.findElement(By.xpath(" //input[@id='life' and @type='email']"));
+        TypeEmail.sendKeys("senoldogan2018@gmail.com");
+        MyFunc.Bekle(1);
+
+        WebElement TypePassword = driver.findElement(By.xpath("(//input[@class='t' and @type='password'])[1]"));
+        TypePassword.sendKeys("Senol1988++");
+        MyFunc.Bekle(1);
+
+        WebElement RememberMeOff = driver.findElement(By.xpath("//input[@class='c' and @type='checkbox']"));
+        RememberMeOff.click();
+        MyFunc.Bekle(1);
+
+        WebElement loginClcik = driver.findElement(By.xpath("//input[@type='submit' and @class='s' and @value='Giriş yap']"));
+        loginClcik.click();
+
+        WebElement verifaction= driver.findElement(By.xpath("//a[text()='Senol']"));
+
+        Assert.assertTrue(verifaction.getText().contains("Senol"));
+    }
+}
